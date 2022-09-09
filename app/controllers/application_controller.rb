a1d6feacce9eccfb10d6c29b2d8e48895b230826
Omar Mahat <omar.mahat@student.moringaschool.com>
2022-09-09 19:31:31 +0300

    Push to heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,25 @@ class ApplicationController < Sinatra::Base
         books.to_json(include: :books)
     end
 
+    get '/books/only' do
+        books = Book.all
+        books.to_json
+    end
+
+    post '/books/only' do
+        book1 = Book.create(
+            title: params[:title],
+            thumbnail: params[:thumbnail],
+            pagecount: params[:pagecount],
+            author_id: params[:author_id],
+        )
+        book1.to_json
+    end
+
+    delete '/books/only/:id' do
+        deleted = Book.find(params[:id])
+        deleted.destroy
+        deleted.to_json
+    end
+
 end
